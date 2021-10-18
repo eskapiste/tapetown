3353f6044bf38df30ad633e7f86401d02b76eb79
Vladimir Zaychikov <zaychikov.vl@gmail.com>
2021-10-18 22:52:59 +0300

    обновление зависимостей

--- src/services/constants.js
+++ src/services/constants.js
@@ -1,6 +1,6 @@
 export const PLAYER_URLS = {
-    STREAM_URL: 'https://papetown.live/radio/8000/stream',
-    META_URL: 'https://papetown.live/api/live/nowplaying/tapetown'
+    STREAM_URL: 'https://swapetown.xyz/radio/8000/stream',
+    META_URL: 'https://swapetown.xyz/api/live/nowplaying/tapetown'
 };
 
 export const SOUNDCLOUD_URL = 'https://cors-anywhere.herokuapp.com/https://feeds.soundcloud.com/users/soundcloud:users:570524325/sounds.rss';
